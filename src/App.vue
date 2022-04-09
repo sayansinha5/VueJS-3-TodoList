1608d902895676f5e6f60b4ed7b865d022be938b
Sayan Sinha <sayansinha5@gmail.com>
2022-04-09 16:13:08 +0530

    Added Vue Functionality: Add New Todo, Mark as Complete/Incomplete, Remove All Todo

--- src/App.vue
+++ src/App.vue
@@ -7,15 +7,15 @@
           <h1 class="text-5xl font-semibold text-gray-600">Todo List</h1>
 
           <div class="mt-6">
-            <form>
+            <form @submit.prevent="addTodo()">
               <div class="grid grid-cols-1 gap-4">
                 <div>
-                  <input class="border-2 outline-none py-2 px-2 shadow-md font-medium w-full rounded border-blue-300 hover:border-blue-600 focus:border-blue-600 focus:shadow-blue-200" />
+                  <input class="border-2 outline-none py-2 px-2 shadow-md font-medium w-full rounded border-blue-300 hover:border-blue-600 focus:border-blue-600 focus:shadow-blue-200" v-model="newTodo" />
                 </div>
                 <div>
                   <div class="grid grid-cols-2 gap-2">
                     <div>
-                      <button class="text-white py-2 px-4 shadow-md w-full rounded bg-red-400 hover:bg-red-600 font-semibold">Remove All Todos</button>
+                      <button class="text-white py-2 px-4 shadow-md w-full rounded bg-red-400 hover:bg-red-600 font-semibold" v-if="todos.length !== 0" @click="removeAllTodos">Remove All Todos</button>
                     </div>
                     <div>
                       <button class="text-white py-2 px-4 shadow-md w-full rounded bg-blue-400 hover:bg-blue-600 font-semibold">Add Todo</button>
@@ -30,15 +30,12 @@
             <div id="todoList">
               <div class="grid grid-cols-1 gap-3">
                 <div>
-                  <div class="h-full">
+                  <div class="h-full" v-if="todos.length === 0">
                     <p class="text-gray-400">It appears you didn't added any To-Do.</p>
                   </div>
                 </div>
-                <div class="rounded shadow-md p-3 h-full hover:shadow-gray-400 text-gray-600 text-lg font-semibold completed">
-                  <span class="text-gray-600 text-lg font-semibold">Complete Homework</span>
-                </div>
-                <div class="rounded shadow-md p-3 h-full hover:shadow-gray-400 text-gray-600 text-lg font-semibold">
-                  <span>Riding Bicycle</span>
+                <div class="rounded shadow-md p-3 h-full hover:shadow-gray-400 text-gray-600 text-lg font-semibold" :class="{ completed: todo.complete }" v-for="(todo, index) in todos" :key="index" @click="completedTodo(todo)">
+                  <span class="text-gray-600 text-lg font-semibold">{{ todo.text }}</span>
                 </div>
               </div>
             </div>
@@ -52,6 +49,34 @@
 </template>
 
 <script setup>
+
+  import { ref } from "vue";
+  
+  const newTodo = ref("");
+  // const initialData = [
+  //   {
+  //     complete: false,
+  //     text: "Create todo list functionality"
+  //   }
+  // ];
+  
+  const todos = ref([]);
+  
+  function addTodo() {
+    
+    if(newTodo.value !== "") {
+      todos.value.push({ complete: false, text: newTodo.value });
+      newTodo.value = "";
+    }
+  }
+  
+  function removeAllTodos() {
+    todos.value.splice(0, todos.value.length);
+  }
+  
+  function completedTodo(todo) {
+    todo.complete = !todo.complete;
+  }
 </script>
 
 <style>
